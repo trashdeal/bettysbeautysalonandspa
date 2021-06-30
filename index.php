374cb668df20fbd5c17fd783b69d78c2d8cbf68c
trashdeal <84789052+trashdeal@users.noreply.github.com>
2021-06-30 22:39:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
     include_once("index.html");
+    include_once("database.php");
+    if(isset($_POST['save']))
+    {	 
+         $first_name = $_POST['formName'];
+         $mobile_no = $_POST['mobileno'];
+         $query = "INSERT INTO appointment(name,mobileno) 
+         values ('$formName','$mobile_no')";
+         if($result = pg_query($query)){
+            echo "Data Added Successfully.";
+         }
+         else{
+            echo "Error.";
+         }
+    }
 ?>
